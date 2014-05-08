bd0a99a62b3c0a7a4e9f44d51473d6e949987380
Nathanaël Courant <ncourant@alt-r06p02.prolo>
2014-05-08 14:39:32 +0200

    Initial commit

---
+++ prologin.cc
@@ -0,0 +1,56 @@
+///
+// This file has been generated, if you wish to
+// modify it in a permanent way, please refer
+// to the script file : gen/generator_cxx.rb
+//
+
+#include "prologin.hh"
+
+///
+// Fonction appelée au début de la partie
+//
+void partie_debut()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appelée pendant la phase de construction
+//
+void phase_construction()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appelée pendant la phase de déplacement
+//
+void phase_deplacement()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appelée pendant la phase de tirs des tourelles
+//
+void phase_tirs()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appelée pendant la phase de siège des tourelles
+//
+void phase_siege()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appelée à la fin de la partie
+//
+void partie_fin()
+{
+  // fonction a completer
+}
+
